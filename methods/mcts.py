1ce5a019500a5ba1f2b8a2832880cc2919ecea47
Agustinus Kristiadi <agustinus@kristia.de>
2025-03-05 14:47:26 -0500

    Add MCTS

--- methods/mcts.py
+++ methods/mcts.py
@@ -13,20 +13,20 @@ np.random.seed(123)
 @dataclass
 class MCTSNode:
     real_node: Node
-    mean_val: float = 0.0
-    n_visits: int = 1
+    total_val: float = 0.0
+    n_visits: int = 0
 
     def __hash__(self) -> int:
         return hash(self.real_node.name)
 
 
 def mcts(
-    tree: nx.DiGraph, root: Node, n_iters: int = 50, gamma: float = math.sqrt(2)
+    tree: nx.DiGraph,
+    root: Node,
+    n_iters: int = 5000,
+    gamma: float = math.sqrt(2),
+    max_child_to_expand: int = 5,
 ) -> Tuple[List[Node], float]:
-    curr: Node = root
-    paths: List[List[Node]] = []
-    rewards: List[float] = []
-
     mcts_tree = nx.DiGraph()
     mcts_root = MCTSNode(real_node=root)
     mcts_tree.add_node(mcts_root)
@@ -34,16 +34,19 @@ def mcts(
     def select(node: MCTSNode) -> MCTSNode:
         if mcts_tree.out_degree(node) == 0:
             # Frontier node
-            node.n_visits += 1
             return node
 
         best_node = node
         best_uct = -np.inf
 
         for child in mcts_tree.successors(node):
-            uct = child.mean_val + gamma * math.sqrt(
-                math.log(node.n_visits) / child.n_visits
-            )
+            # Always visit an unvisited node at least once
+            if child.n_visits == 0:
+                uct = np.inf
+            else:
+                uct = (child.total_val / child.n_visits) + gamma * math.sqrt(
+                    math.log(node.n_visits) / child.n_visits
+                )
 
             if uct > best_uct:
                 best_uct = uct
@@ -51,29 +54,69 @@ def mcts(
 
         return select(best_node)
 
-    def expand(node: MCTSNode):
-        children = tree.successors(node.real_node)
+    def expand(node: MCTSNode) -> MCTSNode:
+        children = list(tree.successors(node.real_node))
+
+        # Restrict the width of the search tree, to make sure the search
+        # progresses downward (doesn't become like BFS)
+        if len(children) > max_child_to_expand:
+            children = np.random.choice(
+                children, size=max_child_to_expand, replace=False
+            )
 
         for child in children:
             mcts_child = MCTSNode(child)
             mcts_tree.add_node(mcts_child)
             mcts_tree.add_edge(node, mcts_child)
 
-    def rollout(mcts_node: MCTSNode) -> float:
-        return 0
+        if node.real_node.is_leaf:
+            return node
+        else:
+            return np.random.choice(list(mcts_tree.successors(node)))
+
+    def rollout(node: MCTSNode) -> float:
+        real_node = node.real_node
+
+        while not real_node.is_leaf:
+            real_node = np.random.choice(list(tree.successors(real_node)))
+
+        return real_node.reward
 
     def backup(node: MCTSNode, reward: float):
-        pass
+        node.total_val += reward
+        node.n_visits += 1
 
+        if mcts_tree.in_degree(node) != 0:
+            backup(list(mcts_tree.predecessors(node))[0], reward)
+
+    # Do search
     for _ in range(n_iters):
         node = select(mcts_root)
-        expand(node)
-        # reward = rollout(node)
-        # backup(node, reward)
+        node = expand(node)
+        reward = rollout(node)
+        backup(node, reward)
 
-    best_idx = np.argmax(rewards)
+    # Get best path
+    curr: MCTSNode = mcts_root
+    path: List[Node] = [curr.real_node]
 
-    return paths[best_idx], rewards[best_idx]
+    while True:
+        children = list(mcts_tree.successors(curr))
+
+        if len(children) == 0:
+            break
+
+        best_idx = np.argmax(
+            [
+                child.total_val / child.n_visits
+                for child in children
+                if child.n_visits > 0
+            ]
+        )
+        curr = children[best_idx]
+        path.append(curr.real_node)
+
+    return path, path[-1].reward
 
 
 # Sample an MDP
@@ -90,6 +133,8 @@ tree, root, best_reward = sample_mdp(
 print(f"Tree: {tree}; Best reward: {best_reward:.3f}")
 
 # Run the search
-res_path, res_reward = mcts(tree, root)
-
-print(f"[MCTS] Path length: {len(res_path)}, Reward: {res_reward:.3f}")
+n_iters = 5000
+res_path, res_reward = mcts(tree, root, n_iters=n_iters, max_child_to_expand=10)
+print(
+    f"[MCTS n_iters={n_iters}] Path length: {len(res_path)}, Reward: {res_reward:.3f}"
+)
